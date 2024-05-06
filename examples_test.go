c74f283584a65c68adbe43a9f138fa89f60cc46f
Googol Lee <i@googol.im>
2024-05-06 12:29:08 +0200

    Update example.

--- examples_test.go
+++ examples_test.go
@@ -1,69 +1,34 @@
 package cfg_test
 
 import (
-	"encoding/json"
-	"strings"
-	"testing"
-
-	"gopkg.in/yaml.v3"
+	"flag"
+	"fmt"
 
 	"github.com/googollee/go-cfg"
 )
 
-func TestLoadJSON(t *testing.T) {
-	type Config struct {
-		Key1  string `cfg:"key1"`
-		Key2  int    `cfg:"key2"`
-		Embed struct {
-			Key string `cfg:"key"`
-		} `cfg:"inner"`
-	}
-
-	strConfig := `{'key1':'value1', 'key2':10, 'inner':{'key': 'value'}}`
-
-	var config Config
-	if err := cfg.Load(strings.NewReader(strConfig), &config, cfg.F("yaml", yaml.NewDecoder), cfg.F("json", json.NewDecoder)); err != nil {
-		panic(err)
-	}
-
-	if got, want := config.Key1, "value1"; got != want {
-		t.Errorf("config.Key1 = %q, want: %q", got, want)
-	}
-	if got, want := config.Key2, 10; got != want {
-		t.Errorf("config.Key2 = %v, want: %v", got, want)
-	}
-	if got, want := config.Embed.Key, "value"; got != want {
-		t.Errorf("config.Embed.Key = %q, want: %q", got, want)
-	}
+type Config struct {
+	Str string `cfg:"str"`
+	Int int    `cfg:"int"`
 }
 
-func TestLoadYAML(t *testing.T) {
-	type Config struct {
-		Key1  string `cfg:"key1"`
-		Key2  int    `cfg:"key2"`
-		Embed struct {
-			Key string `cfg:"key"`
-		} `cfg:"inner"`
-	}
+func ExampleConfig() {
+	fromFlags := cfg.FromFlags[Config]("demo_")
 
-	strConfig := `key1: value1
-key2: 10
-inner:
-  key: value
-`
+	cfgFile := flag.String("config", "./config.yaml", "config file")
+	help := flag.Bool("help", false, "show help")
+	flag.Parse()
+
+	if *help {
+		flag.Usage()
+		return
+	}
 
 	var config Config
-	if err := cfg.Load(strings.NewReader(strConfig), &config, cfg.F("yaml", yaml.NewDecoder), cfg.F("json", json.NewDecoder)); err != nil {
-		panic(err)
+	if err := cfg.Parse(&config, cfg.FromFile(*cfgFile /* optional file formats: cfg.YAML, cfg.JSON, cfg.TOML */), cfg.FromEnv("DEMO_"), fromFlags); err != nil {
+		fmt.Println("parse config file %q error: %v", *cfgFile, err)
+		return
 	}
 
-	if got, want := config.Key1, "value1"; got != want {
-		t.Errorf("config.Key1 = %q, want: %q", got, want)
-	}
-	if got, want := config.Key2, 10; got != want {
-		t.Errorf("config.Key2 = %v, want: %v", got, want)
-	}
-	if got, want := config.Embed.Key, "value"; got != want {
-		t.Errorf("config.Embed.Key = %q, want: %q", got, want)
-	}
+	fmt.Println("config:", config)
 }
