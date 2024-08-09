9f776e6c863095cbc6110f0fec9594719ce74df2
Googol Lee <i@googol.im>
2024-08-09 14:21:31 +0200

    Update

--- env.go
+++ env.go
@@ -39,23 +39,25 @@ func (s *envSource) Setup(t reflect.Type) error {
 }
 
 func (s *envSource) Parse(ctx context.Context, v any) error {
-	return walkFields(reflect.ValueOf(v), s.prefix, s.splitter, nil, func(meta fieldMeta, v reflect.Value) error {
-		key := strings.ToUpper(meta.FullKey)
+	return walkFields(reflect.ValueOf(v), s.prefix, s.splitter, nil, s.parseField)
+}
 
-		value, ok := os.LookupEnv(key)
-		if !ok {
-			return nil
-		}
+func (s *envSource) parseField(meta fieldMeta, v reflect.Value) error {
+	key := strings.ToUpper(meta.FullKey)
 
-		v = digPtr(v)
-
-		nv := newNullValue(v.Type())
-		if err := nv.UnmarshalText([]byte(value)); err != nil {
-			return fmt.Errorf("can't parse value %q of env %q: %w", value, key, err)
-		}
-
-		nv.CopyTo(v)
-
+	value, ok := os.LookupEnv(key)
+	if !ok {
 		return nil
-	})
+	}
+
+	v = digPtr(v)
+
+	nv := newNullValue(v.Type())
+	if err := nv.UnmarshalText([]byte(value)); err != nil {
+		return fmt.Errorf("can't parse value %q of env %q: %w", value, key, err)
+	}
+
+	nv.CopyTo(v)
+
+	return nil
 }
--- env_test.go
+++ env_test.go
@@ -3,19 +3,21 @@ package cfg
 import (
 	"context"
 	"os"
+	"reflect"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 )
 
 func TestEnvSource(t *testing.T) {
+	type Inner struct {
+		Str string `cfg:"str"`
+		I   int    `cfg:"i"`
+	}
 	type Config struct {
 		Str   string `cfg:"str"`
 		I     int    `cfg:"i"`
-		Inner struct {
-			Str string `cfg:"str"`
-			I   int    `cfg:"i"`
-		} `cfg:"inner"`
+		Inner Inner  `cfg:"inner"`
 	}
 
 	for key, value := range map[string]string{
@@ -28,7 +30,8 @@ func TestEnvSource(t *testing.T) {
 
 	src := FromEnv("test", EnvSplitter("_"))
 	var config Config
-	config.Inner.I = 20
+	config.I = 20       // Env should overwrite this field
+	config.Inner.I = 20 // Env doesn't exist, should keep the value of this field
 	if err := src.Parse(context.Background(), &config); err != nil {
 		t.Fatal("parse error:", err)
 	}
@@ -36,10 +39,7 @@ func TestEnvSource(t *testing.T) {
 	want := Config{
 		Str: "outer_str",
 		I:   10,
-		Inner: struct {
-			Str string `cfg:"str"`
-			I   int    `cfg:"i"`
-		}{
+		Inner: Inner{
 			Str: "inner_str",
 			I:   20,
 		},
@@ -47,5 +47,64 @@ func TestEnvSource(t *testing.T) {
 
 	if diff := cmp.Diff(config, want); diff != "" {
 		t.Errorf("diff:\n%s", diff)
+	}
+}
+
+func TestEnvParseField(t *testing.T) {
+	tests := []struct {
+		input string
+		want  any
+	}{
+		{"str", "str"},
+		{"120", 120},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.input, func(t *testing.T) {
+			meta := fieldMeta{
+				FullKey: "TEST_PARSE_FIELD",
+			}
+			os.Setenv(meta.FullKey, tc.input)
+
+			src := envSource{}
+			got := reflect.New(reflect.TypeOf(tc.want)).Elem()
+
+			if err := src.parseField(meta, got); err != nil {
+				t.Fatal("src.parseField() error:", err)
+			}
+			if !reflect.DeepEqual(got.Interface(), tc.want) {
+				t.Errorf("src.parseField() = %v, want: %v", got.Interface(), tc.want)
+			}
+		})
+	}
+}
+
+func TestEnvParseFieldError(t *testing.T) {
+	tests := []struct {
+		input string
+		want  any
+		err   string
+	}{
+		{"str", 120, `can't parse value "str" of env "TEST_PARSE_FIELD": convert "str" to int error: strconv.ParseInt: parsing "str": invalid syntax`},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.input, func(t *testing.T) {
+			meta := fieldMeta{
+				FullKey: "TEST_PARSE_FIELD",
+			}
+			os.Setenv(meta.FullKey, tc.input)
+
+			src := envSource{}
+			got := reflect.New(reflect.TypeOf(tc.want)).Elem()
+
+			err := src.parseField(meta, got)
+			if err == nil {
+				t.Fatal("src.parseField() should return an error, but not")
+			}
+			if diff := cmp.Diff(tc.err, err.Error()); diff != "" {
+				t.Errorf("src.parseField() error diff:\n%s", diff)
+			}
+		})
 	}
 }
