728f0b468643699d11b3ffd6f9971f2871bc773c
Googol Lee <i@googol.im>
2024-11-23 20:15:46 +0100

    Update examples.

--- config.go
+++ config.go
@@ -17,10 +17,52 @@ type Initializer interface {
 }
 
 // RegisterInitializer registers an Initializer instance `value` with the `name` as the scope name. [Init] function parses configuration from a file, environment or flags, stores into `value`, then call `value.Init()`. The `value` provider could initialize global instances in `value.Init()`.
-func RegisterInitializer(name string, value Initializer) {}
+//
+// Example:
+//
+// ```go
+//
+//	package subpack
+//
+//	type config struct {
+//		Int int `cfg:"int"`
+//	}
+//
+//	func (c *config) Init(ctx context.Context) error {
+//		singletonInstance = c.Int
+//	}
+//
+// var singletonInstance int
+//
+//	func init() {
+//		cfg.RegisterInitializer[config]("subpack")
+//	}
+//
+// ```
+func RegisterInitializer[T Initializer](name string) {}
 
 // RegisterValue registers an instance `value` with the `name` as the scope name and returns a function to get parsed `value` from the context. [Init] function parses configuration from a file, environment or flags, stores into `value`, then call `value.Init()`.
-func RegisterValue[T any](name string, value *T) (getter func(ctx context.Context) *T) {
+//
+// Example:
+//
+// ```go
+//
+//	package main
+//
+//	func main() {
+//		dbConfig := cfg.RegisterValue[db.Config]("database")
+//
+//		ctx, err := cfg.Init(context.Background())
+//		if err != nil {
+//			panic(err)
+//		}
+//
+//		db := database.New(dbConfig(ctx))
+//
+//	}
+//
+// ```
+func RegisterValue[T any](name string) (getter func(ctx context.Context) *T) {
 	return
 }
 
