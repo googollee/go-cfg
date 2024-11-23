520aa10a44cb05d53e826fd7c8caa2813d0c6102
Googol Lee <i@googol.im>
2024-11-23 17:14:41 +0100

    Try a new init framework.

--- config.go
+++ config.go
@@ -1,39 +1,24 @@
+// package cfg implements a framework to load/parse configuration from a file, environment or flags.
 package cfg
 
-import (
-	"context"
-	"reflect"
-)
+import "context"
 
-type Parser[T any] struct {
-	typ     reflect.Type
-	sources []Source
+// Initializer is the interface that wraps a instance that provides configuration information and saves the value provided by the configuration.
+//
+// The `cfg` library parses values from a file, environment or flags and stores into registered instances. If the instance is an Initializer, the method `Initializer.Init()` is called after storing values. The Initializer provider could initialize global instances in this method.
+type Initializer interface {
+	Init(ctx context.Context) error
 }
 
-func Parse[T any](src ...Source) *Parser[T] {
-	var v T
-	t := reflect.TypeOf(v)
+// RegisterInitializer registers an Initializer instance `value` with the `name` as the scope name. [Init] function parses configuration from a file, environment or flags, stores into `value`, then call `value.Init()`. The `value` provider could initialize global instances in `value.Init()`.
+func RegisterInitializer(name string, value Initializer) {}
 
-	ret := &Parser[T]{
-		typ:     t,
-		sources: append([]Source{fromDefault()}, src...),
-	}
-
-	for _, src := range ret.sources {
-		if err := src.Setup(t); err != nil {
-			panic(err)
-		}
-	}
-
-	return ret
+// RegisterValue registers an instance `value` with the `name` as the scope name and returns a function to get parsed `value` from the context. [Init] function parses configuration from a file, environment or flags, stores into `value`, then call `value.Init()`.
+func RegisterValue[T any](name string, value *T) (getter func(ctx context.Context) *T) {
+	return
 }
 
-func (p *Parser[T]) Parse(ctx context.Context, v *T) error {
-	for _, src := range p.sources {
-		if err := src.Parse(ctx, v); err != nil {
-			return err
-		}
-	}
-
-	return nil
+// Init parses configuration from a file, environment or flags. It returns a new context which could be used to retreive values registered with [RegisterValue].
+func Init(ctx context.Context) (context.Context, error) {
+	return nil, nil
 }
